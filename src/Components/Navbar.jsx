01b3aeac533515ca28d6fbd72f91cbaaf3afb5b6
CamiloProg <camilom200107@gmail.com>
2024-10-28 08:18:38 -0500

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,29 +1,28 @@
-import { Link } from 'react-router-dom'
-import ThemeButton from './ThemeButton'
-import { useCharStates } from '../Context'
-
+import { Link } from "react-router-dom";
+import { useCharStates } from "../Context";
+import logo from "../assets/0-Logo/isologo.svg";
+import Button from "./Button";
 const Navbar = () => {
   const { state } = useCharStates();
   return (
     // dark-nav es una clase que no existe, se debe estilar para el tema
-    <nav className={`flex justify-evenly   
-        ${!state.theme && "dark-nav"}`}>
-    <div className='text-3xl font-bold underline'>
-      <span>Logo</span>
-    </div>
-    <div className='flex'>
-      <ul className='flex'>
-        <li>
-          <Link to="/">Inicio</Link> 
-        </li>
-        <li>
-          <Link to="/favs">Favoritos</Link> 
-        </li>
-      </ul>
-      <ThemeButton />
-    </div>
-  </nav>
-  )
-}
+    <nav
+      className={`flex justify-between items-center w-full py-3 md:px-10 px-2 bg-black  
+        ${!state.theme && "dark-nav"}`}
+    >
+      <div className='md:flex items-end md:gap-4'>
+        <Link to='/'>
+          <img className='w-24 md:w-full' src={logo} alt='' />
+        </Link>
+        <h3 className='text-primaryBlue'>Tu boleto a la diversión</h3>
+      </div>
+      <div className='flex md:gap-5 gap-2'>
+        <Button type='secondary'>Iniciar sesión</Button>
+        <Button to='/favs'>Crear cuenta</Button>
+        {/* <ThemeButton /> */}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/Components/ThemeButton.jsx
+++ src/Components/ThemeButton.jsx
@@ -1,18 +1,21 @@
 import { useCharStates } from "../Context";
 
- const ThemeButton = () => {
-    const { state, dispatch } = useCharStates();
-    const toggleTheme = () => dispatch({
-      type: 'TOGGLE_THEME',
+const ThemeButton = () => {
+  const { state, dispatch } = useCharStates();
+  const toggleTheme = () =>
+    dispatch({
+      type: "TOGGLE_THEME",
       payload: !state.theme,
     });
 
-    return (
-      <button className={`btn-theme ${state.theme ? "btn-light" : "btn-dark"}`} 
-              onClick={toggleTheme} >
-        {state.theme ? '🌞' : '🌑'}
-      </button>
-    );
-}
+  return (
+    <button
+      className={`btn-theme bg-black ${state.theme ? "btn-light" : "btn-dark"}`}
+      onClick={toggleTheme}
+    >
+      {state.theme ? "🌞" : "🌑"}
+    </button>
+  );
+};
 
 export default ThemeButton;
