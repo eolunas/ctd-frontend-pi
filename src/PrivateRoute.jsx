f253b65748f25bae91ae2b802839348f931942c9
CamiloProg <camilom200107@gmail.com>
2024-11-20 16:22:56 -0500

    privateroute fix

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -2,12 +2,14 @@ import { Navigate } from "react-router-dom";
 import { useCharStates } from "./Context";
 
 const PrivateRoute = ({ children }) => {
-  const { state } = useCharStates();
+  const { state, loading } = useCharStates();
 
   console.log(state.user?.role);
-
+  if (loading) {
+    return <div>Cargando...</div>; // O un componente de loader más sofisticado
+  }
   // Si el usuario no está autenticado (estado `isLoggedIn` es falso) o no tiene el rol 'Administrator'
-  if (!state.isLoggedIn || state.user?.role !== "Administrator") {
+  if (!state?.isLoggedIn || state.user?.role !== "Administrator") {
     return <Navigate to='/unauthorized' />;
   }
 
